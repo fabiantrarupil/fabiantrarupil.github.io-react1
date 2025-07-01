fecd129421bc6149ad3ec83f77c699f75e1c8274
FTWebDeveloper <ftwebdeveloper@gmail.com>
2025-07-01 00:19:48 -0400

    feat: Implementación de Formularios con condiciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import RegisterPage from './components/RegisterPage'; // Importar RegisterPage
-import LoginPage from './components/LoginPage';       // Importar LoginPage
+import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
 import './App.css';
 
 function App() {
-  // Para la Tarea II, comenta Home y descomenta RegisterPage o LoginPage según necesites probar.
-  // Primero, descomenta RegisterPage para probar el formulario de registro.
-  // Luego, comenta RegisterPage y descomenta LoginPage para probar el de login.
 
   return (
     <>
