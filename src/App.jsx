bf40310c93dab8114736801f03961da513a4e1c0
FTWebDeveloper <ftwebdeveloper@gmail.com>
2025-07-01 00:15:53 -0400

    feat: Implementación de Formularios

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import './App.css'; 
+import RegisterPage from './components/RegisterPage'; // Importar RegisterPage
+import LoginPage from './components/LoginPage';       // Importar LoginPage
+import './App.css';
 
 function App() {
+  // Para la Tarea II, comenta Home y descomenta RegisterPage o LoginPage según necesites probar.
+  // Primero, descomenta RegisterPage para probar el formulario de registro.
+  // Luego, comenta RegisterPage y descomenta LoginPage para probar el de login.
+
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+
+      <RegisterPage />
+
+      <LoginPage />
+
       <Footer />
     </>
   );
